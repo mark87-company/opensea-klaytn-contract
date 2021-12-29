886cf157775f560811eecd804116600550621624
Asher Hwang <dev@hsh.kr>
2021-12-29 09:20:59 +0900

    Fetch submodule

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,2 +1,6 @@
 pragma solidity ^0.5.0;
+
+import '../MAS_contracts/Klaytn/bank.sol';
+import '../MAS_contracts/Klaytn/factory.sol';
 import '../MAS_contracts/Klaytn/MAS_Sales.sol';
+import '../MAS_contracts/Klaytn/whiteLists.sol';
